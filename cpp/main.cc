e546de1923efad13a06dfc4d231afd6084c50994
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-11-28 20:51:42 +0100

    [CPP] Added a cpp implementation

---
+++ cpp/main.cc
@@ -0,0 +1,59 @@
+#include <array>
+#include <stdint.h>
+#include <iostream>
+#include <chrono>
+#include <iterator>
+
+bool is_done(std::array<char, kmer_length> &chars)
+{
+  for (auto c : chars)
+  {
+    if (c != 'T')
+    {
+      return false;
+    }
+  }
+  return true;
+}
+
+int8_t convert(int8_t value)
+{
+  switch (value)
+  {
+  case 'A':
+    return 'C';
+  case 'C':
+    return 'G';
+  case 'G':
+    return 'T';
+  case 'T':
+    return 'A';
+  }
+  return ' ';
+}
+
+int main()
+{
+  typedef std::chrono::high_resolution_clock Clock;
+  auto t1 = Clock::now();
+  auto chars = std::array<char, kmer_length>();
+  chars.fill('A');
+  int64_t counter = 1;
+  while (!is_done(chars))
+  {
+    counter += 1;
+    for (int i = 0; i < kmer_length; i++)
+    {
+      char c = chars[i];
+      chars[i] = convert(c);
+      // std::cout << std::string(std::begin(chars), std::end(chars)) << std::endl;
+      if (c != 'T')
+      {
+        break;
+      }
+    }
+  }
+  auto t2 = Clock::now();
+  auto delta = ((t2 - t1).count() / 1000) / 1000;
+  std::cout << "Nummer of generated k-mers: " << counter << " - took " << delta << "ms" << std::endl;
+}
