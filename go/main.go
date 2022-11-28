8a899c81a2460974cc8bfb831ff7ded4d3c5697f
worldOneo <62426504+worldOneo@users.noreply.github.com>
2022-11-28 22:01:56 +0100

    [Go] Added a Go implementation

---
+++ go/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+)
+
+func convert(c byte) byte {
+	switch c {
+	case 'A':
+		return 'C'
+	case 'C':
+		return 'G'
+	case 'G':
+		return 'T'
+	case 'T':
+		return 'A'
+	}
+	return ' '
+}
+
+const lenth = 15
+
+func main() {
+	start := time.Now()
+	base := "ACGT"
+	end := base[len(base) - 1]
+	s := ""
+	for i := 0; i < lenth; i++ {
+		s += string(base[0])
+	}
+	sLast := ""
+	for i := 0; i < lenth; i++ {
+		sLast += string(end)
+	}
+	ss := []byte(s)
+	ssLast := []byte(sLast)
+	counter := uint64(1)
+	for !bytes.Equal(ss, ssLast) {
+		counter++
+		fmt.Println(string(ss))
+		for i := 0; i < lenth; i++ {
+			old := ss[i]
+			ss[i] = convert(old)
+			if old != end {
+				break
+			}
+		}
+	}
+	delta := time.Since(start)
+	fmt.Printf("Number of generated k-mers: %v - took %v\n", counter, delta)
+}
