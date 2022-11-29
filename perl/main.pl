d8e2a6ef6ce9e9c8052f3460077744e6d374d1d5
Koni vKxni <eventbeate@gmail.com>
2022-11-29 13:42:48 +0100

    raped perl down to 14s (from 40)

--- perl/main.pl
+++ perl/main.pl
@@ -1,52 +1,28 @@
-use strict;
-use warnings;
-
-sub convert {
-    my $c = shift;
-    if ($c eq 'A') {
-        return 'C';
-    }
-    if ($c eq 'C') {
-        return 'G';
-    }
-    if ($c eq 'G') {
-        return 'T';
-    }
-    if ($c eq 'T') {
-        return 'A';
-    }
-}
+my %map = (
+    A => 'C',
+    C => 'G',
+    G => 'T',
+    T => 'A',
+);
 
 my $start = time();
 print "Start
 
 ";
 
-my $opt = "ACGT";
-my $s = "";
-my $s_last = "";
+my @opt = split //, "ACGT";
 my $len_str = 13;
+my $s = $opt[0] x $len_str;
+my $s_last = $opt[-1] x $len_str;
+my $last_char = $opt[-1];
 
-for (my $i = 0; $i < $len_str; $i++) {
-    $s .= substr($opt, 0, 1);
-}
-
-for (my $i = 0; $i < $len_str; $i++) {
-    $s_last .= substr($opt, -1, 1);
-}
-
-my $pos = 0;
 my $counter = 1;
 while ($s ne $s_last) {
     $counter += 1;
-    for (my $i = 0; $i < $len_str; $i++) {
-        if (substr($s, $i, 1) eq substr($opt, -1, 1)) {
-            $s = substr($s, 0, $i).convert(substr($s, $i, 1)).substr($s, $i + 1);
-        }
-        else {
-            $s = substr($s, 0, $i).convert(substr($s, $i, 1)).substr($s, $i + 1);
-            last;
-        }
+    for my $idx (0 .. $len_str) {
+        my $char = substr $s, $idx, 1;
+        substr $s, $idx, 1, $map{$char};
+        last if $char ne $last_char;
     }
 }
 
