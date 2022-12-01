ede2fd84f4d1d3e921d86b6c0353f0ee9ae440a4
Flawlesscode <37778817+FlawlessDeveloper@users.noreply.github.com>
2022-12-01 18:28:44 +0100

    Port c++ to rust

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,40 +1,52 @@
 use std::time::Instant;
 
+const KMER_LENGHT: usize = if cfg!(feature = "1kmers") {
+    1
+} else if cfg!(feature = "2kmers") {
+    2
+} else if cfg!(feature = "3kmers") {
+    3
+} else if cfg!(feature = "4kmers") {
+    4
+} else if cfg!(feature = "5kmers") {
+    5
+} else if cfg!(feature = "6kmers") {
+    6
+} else if cfg!(feature = "7kmers") {
+    7
+} else if cfg!(feature = "8kmers") {
+    8
+} else if cfg!(feature = "9kmers") {
+    9
+} else if cfg!(feature = "10kmers") {
+    10
+} else if cfg!(feature = "11kmers") {
+    11
+} else if cfg!(feature = "12kmers") {
+    12
+} else if cfg!(feature = "13kmers") {
+    13
+} else if cfg!(feature = "14kmers") {
+    14
+} else if cfg!(feature = "15kmers") {
+    15
+} else if cfg!(feature = "16kmers") {
+    16
+} else if cfg!(feature = "17kmers") {
+    17
+} else if cfg!(feature = "18kmers") {
+    18
+} else if cfg!(feature = "19kmers") {
+    19
+} else {
+    15
+};
+
 fn main() {
     // get current nano time
     let start = Instant::now();
 
-    let opt = "ACGT";
-    let end = opt.chars().last().unwrap();
-    let len_str = std::env::args().nth(1).unwrap().parse::<usize>().unwrap();
-    let mut s = String::with_capacity(len_str);
-    let mut s_last = String::with_capacity(len_str);
-
-    let first = opt.chars().nth(0).unwrap().to_string();
-    let last = opt.chars().last().unwrap().to_string();
-
-    for _ in 0..len_str {
-        s += &first;
-        s_last += &last;
-    }
-
-    let s_last: Vec<char> = s_last.chars().collect();
-    let mut s: Vec<char> = s.chars().collect();
-    let mut counter: i128 = 1;
-
-    while !s.eq(&s_last) {
-        counter += 1;
-
-        // println!("kmer: {} ", s);
-        for i in 0..len_str {
-            let c: char = s[i];
-            let new = convert(c);
-            s[i] = new;
-            if !c.eq(&end) {
-                break;
-            }
-        }
-    }
+    let counter = new_run();
 
     let duration = start.elapsed();
     println!(
@@ -42,6 +54,23 @@ fn main() {
         counter,
         duration.as_millis()
     );
+}
+
+fn new_run() -> i128 {
+    let mut chars = ['A'; KMER_LENGHT];
+    let mut counter: i128 = 1;
+    while !is_done(&chars) {
+        counter += 1;
+        //println!("Begin {} End {}", chars[0], chars[KMER_LENGHT - 1]);
+        for i in 0..KMER_LENGHT {
+            let c = chars[i];
+            chars[i] = convert(c);
+            if c != 'T' {
+                break;
+            }
+        }
+    }
+    counter
 }
 
 fn convert(c: char) -> char {
@@ -52,4 +81,13 @@ fn convert(c: char) -> char {
         'T' => 'A',
         _ => ' ',
     };
+}
+
+fn is_done(chars: &[char]) -> bool {
+    for &c in chars {
+        if c != 'T' {
+            return false;
+        }
+    }
+    return true;
 }
