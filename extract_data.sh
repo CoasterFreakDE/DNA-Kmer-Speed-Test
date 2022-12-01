96605430f4bfcc36ab4d4bfe573a1e8c77e0c480
Xanax <moritz.fuessel@flawcra.cc>
2022-12-01 21:44:14 +0100

    Update extract_data.sh

--- extract_data.sh
+++ extract_data.sh
@@ -1,1 +1,1 @@
-echo $(cat results/$1_$2/kmer_$1_$2.rawresult.txt | ./sd "(.*)\nNumber of generated k-mers: (.*) took " "")
+echo $(cat results/$1_$2/kmer_$1_$2.rawresult.txt | sed -e '/.* - took .*/!d' | sed -r 's/Number of generated k-mers: .* - took (.*)/\1/g')
